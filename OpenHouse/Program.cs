9c71e32d7738dd5671571cc7833b29fd932f6186
Chip <chip.elmer@gmail.com>
2018-10-25 13:10:12 -0500

    Added separate methods for the realtor speaking and ACE speaking, with each one handling the changing of the console color accordingly.

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -5,11 +5,6 @@ namespace OpenHouse
 {
     class Program
     {
-        public static void SpeakingDelay()
-        {
-            Thread.Sleep(4000);
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Open house\nAugust 15th 2008\n4:00pm\n1007 Mountian Drive,Gotham");
@@ -46,45 +41,38 @@ namespace OpenHouse
                     Console.Clear();
                 }
 
-                SpeakThenClear("Welcome To The Stately Wayne Manor\nLocated in the Crest Hill Community approximately 12 miles outside of Gotham City\nAt 42,500 it is slightly smaller than The Gotham Rouges football field.");
-                SpeakThenClear("The Estate is the current home of Bruce Wayne\nThe heir to Wayne Industries the most prominent tech firm in the world.");
-                SpeakThenClear("The palacial residence pertains 11rooms and 7bathrooms,\nif on the market today it would appraise for 32,100,000.");
-                SpeakThenClear("Originally Designed by Nathan van Derm as a commission for Darius Wayne in 1795\nThe project was inherited by Darius'sons; Solomon Zebadiah Wayne and Joshua Thomas Wayne in 1858.");
-                SpeakThenClear("While construction was underway the wayne siblings discovered catacombs\nunder the soon to be built estate.");
-                SpeakThenClear("The Waynes exploited their nack for humanatarian efforts,\nThis was one of the last stops on the upper east coast for The Underground Railroad.");
+                RealtorSpeak("Welcome To The Stately Wayne Manor\nLocated in the Crest Hill Community approximately 12 miles outside of Gotham City\nAt 42,500 it is slightly smaller than The Gotham Rouges football field.");
+                RealtorSpeak("The Estate is the current home of Bruce Wayne\nThe heir to Wayne Industries the most prominent tech firm in the world.");
+                RealtorSpeak("The palacial residence pertains 11rooms and 7bathrooms,\nif on the market today it would appraise for 32,100,000.");
+                RealtorSpeak("Originally Designed by Nathan van Derm as a commission for Darius Wayne in 1795\nThe project was inherited by Darius'sons; Solomon Zebadiah Wayne and Joshua Thomas Wayne in 1858.");
+                RealtorSpeak("While construction was underway the wayne siblings discovered catacombs\nunder the soon to be built estate.");
+                RealtorSpeak("The Waynes exploited their nack for humanatarian efforts,\nThis was one of the last stops on the upper east coast for The Underground Railroad.");
                 Console.WriteLine("Fredrick Douglass and Harriet Tubman are included on the list of our many disinguished guests");
             }
             else if (response.ToUpper() == "YES")
             {
                 Console.Clear();
-                SpeakThenClear("Welcome To The Stately Wayne Manor\nLocated in the Crest Hill Community approximately 12 miles outside of Gotham City\nAt 42,500 it is slightly smaller than The Gotham Rouges football field.");
-                SpeakThenClear("The Estate is the current home of Bruce Wayne\nThe heir to Wayne Industries the most prominent tech firm in the world.");
-                SpeakThenClear("The palacial residence pertains 11rooms and 7bathrooms,\nif on the market today it would appraise for 32,100,000.");
-                SpeakThenClear("Originally Designed by Nathan van Derm as a commission for Darius Wayne in 1795\nThe project was inherited by Darius'sons; Solomon Zebadiah Wayne and Joshua Thomas Wayne in 1858.");
-                SpeakThenClear("While construction was underway the wayne siblings discovered catacombs\nunder the soon to be built estate.");
-                SpeakThenClear("The Waynes exploited their nack for humanatarian efforts,\nThis was one of the last stops on the upper east coast for The Underground Railroad.");
+                RealtorSpeak("Welcome To The Stately Wayne Manor\nLocated in the Crest Hill Community approximately 12 miles outside of Gotham City\nAt 42,500 it is slightly smaller than The Gotham Rouges football field.");
+                RealtorSpeak("The Estate is the current home of Bruce Wayne\nThe heir to Wayne Industries the most prominent tech firm in the world.");
+                RealtorSpeak("The palacial residence pertains 11rooms and 7bathrooms,\nif on the market today it would appraise for 32,100,000.");
+                RealtorSpeak("Originally Designed by Nathan van Derm as a commission for Darius Wayne in 1795\nThe project was inherited by Darius'sons; Solomon Zebadiah Wayne and Joshua Thomas Wayne in 1858.");
+                RealtorSpeak("While construction was underway the wayne siblings discovered catacombs\nunder the soon to be built estate.");
+                RealtorSpeak("The Waynes exploited their nack for humanatarian efforts,\nThis was one of the last stops on the upper east coast for The Underground Railroad.");
                 Console.WriteLine("Fredrick Douglass and Harriet Tubman are included on the list of our many disinguished guests");
             }
 
             Console.WriteLine("Please forgive me, I forgot acquire your name?");
             response = Console.ReadLine();
             Console.Clear();
-            SpeakThenClear(response + ", it is a pleasure to meet you.");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            SpeakThenClear("WOOF WOOF, GRRRRRRRRRRRRRRRRR");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("CALM DOWN BEFORE YOU FRIGHTEN OUR GUEST");
-            SpeakingDelay();
-            SpeakThenClear(response + ",This is Archabald Charles Edmondson known as ACE our estate overseer he seems a bit awnry.\nI promise not to let him bite a plug in you");
-            SpeakThenClear("Perhaps a size 12, For snack ACE");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            SpeakThenClear("whimper");
-            Console.ForegroundColor = ConsoleColor.White;
-            SpeakThenClear("Oh Hush");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            SpeakThenClear("WOOF");
-            Console.ForegroundColor = ConsoleColor.White;
-            SpeakThenClear("I wont be accepting any Tom Foolery from you today,\n Now say hello or retire to your quarters");
+            RealtorSpeak(response + ", it is a pleasure to meet you.");
+            AceSpeak("WOOF WOOF, GRRRRRRRRRRRRRRRRR");
+            RealtorSpeak("CALM DOWN BEFORE YOU FRIGHTEN OUR GUEST");
+            RealtorSpeak(response + ",This is Archabald Charles Edmondson known as ACE our estate overseer he seems a bit awnry.\nI promise not to let him bite a plug in you");
+            RealtorSpeak("Perhaps a size 12, For snack ACE");
+            AceSpeak("whimper");
+            RealtorSpeak("Oh Hush");
+            AceSpeak("WOOF");
+            RealtorSpeak("I wont be accepting any Tom Foolery from you today,\n Now say hello or retire to your quarters");
 
             The_BatCave batcave = new The_BatCave();
             batcave.Name = "The Batcave";
@@ -94,6 +82,23 @@ namespace OpenHouse
             Console.WriteLine(batcave.Description);
 
             Console.ReadLine();
+        }
+
+        public static void RealtorSpeak(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            SpeakThenClear(message);
+        }
+
+        public static void AceSpeak(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            SpeakThenClear(message);
+        }
+
+        public static void SpeakingDelay()
+        {
+            Thread.Sleep(4000);
         }
 
         public static void SpeakThenClear(string message)
